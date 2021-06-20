085047e4bc5155a7bd32447b647db7e63dea633d
shutaozhenzhen <3462232313@qq.com>
2021-06-20 22:48:02 +0800

    获取标题

--- app/renderer.js
+++ app/renderer.js
@@ -1,9 +1,22 @@
+const parser =new DOMParser()
 const linksSection = document.querySelector('.links')
 const errorMessage = document.querySelector('.error-message')
 const newLinkForm = document.querySelector('.new-link-form')
 const newLinkUrl = document.querySelector('.new-link-url')
 const newLinkSubmit = document.querySelector('.new-link-submit')
 const clearStorageButton = document.querySelector('.clear-storage')
+const clearForm = () => {
+	newLinkUrl.value = null;
+}
 newLinkUrl.addEventListener('keyup', () => {
 	newLinkSubmit.disabled = !newLinkUrl.validity.valid
+})
+newLinkForm.addEventListener('submit', (event) => {
+	event.preventDefault()
+	const url = newLinkUrl.value
+	fetch(url)
+		.then(reponse => reponse.text())
+		.then(text=>parser.parseFromString(text,'text/html'))
+		.then(nodes=>nodes.querySelector('title').innerText)
+//		.then(console.log)
 })
