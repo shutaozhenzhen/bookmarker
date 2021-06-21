479d72f7aeee9e6ef9943f02a1ac594a039d9369
shutaozhenzhen <3462232313@qq.com>
2021-06-21 20:55:49 +0800

    显示url列表

--- app/renderer.js
+++ app/renderer.js
@@ -1,13 +1,21 @@
-const parser =new DOMParser()
+const parser = new DOMParser()
 const linksSection = document.querySelector('.links')
 const errorMessage = document.querySelector('.error-message')
 const newLinkForm = document.querySelector('.new-link-form')
 const newLinkUrl = document.querySelector('.new-link-url')
 const newLinkSubmit = document.querySelector('.new-link-submit')
 const clearStorageButton = document.querySelector('.clear-storage')
-const clearForm = () => {
-	newLinkUrl.value = null;
+const renderLinks = () => {
+	linksSection.innerHTML = Object.keys(localStorage).map(key => `
+<div class="link">
+<h3>${localStorage.getItem(key)}</h3>
+<p>
+<a href="${key}">${key}</a>
+</p>
+</div>
+`).join('')
 }
+renderLinks()
 newLinkUrl.addEventListener('keyup', () => {
 	newLinkSubmit.disabled = !newLinkUrl.validity.valid
 })
@@ -16,7 +24,14 @@ newLinkForm.addEventListener('submit', (event) => {
 	const url = newLinkUrl.value
 	fetch(url)
 		.then(reponse => reponse.text())
-		.then(text=>parser.parseFromString(text,'text/html'))
-		.then(nodes=>nodes.querySelector('title').innerText)
-//		.then(console.log)
+		.then(text => parser.parseFromString(text, 'text/html'))
+		.then(nodes => nodes.querySelector('title').innerText)
+		.then(title => localStorage.setItem(url, title))
+		.then(() => newLinkUrl.value = null)
+		.then(() => newLinkSubmit.disabled = true)
+		.then(renderLinks)
+})
+clearStorageButton.addEventListener('click', () => {
+	localStorage.clear()
+	renderLinks()
 })
